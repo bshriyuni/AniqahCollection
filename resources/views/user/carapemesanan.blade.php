bca884ef81c9ba24053e75c59bd860ff3568da4a
adeliapuspita <adhepuspita123@gmail.com>
2023-12-21 12:24:19 +0800

    update

--- resources/views/user/carapemesanan.blade.php
+++ resources/views/user/carapemesanan.blade.php
@@ -138,6 +138,7 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"/>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
